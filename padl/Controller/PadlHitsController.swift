882df0d085dc976840653c046ec76e66467305d2
Matthew Feng <mattfeng@mit.edu>
2018-07-09 15:02:34 -0400

    Fix search 3 per row

---
+++ padl/Controller/PadlHitsController.swift
@@ -0,0 +1,14 @@
+//
+//  PadlHitsController.swift
+//  padl
+//
+//  Created by Matthew Feng on 7/7/18.
+//  Copyright © 2018 Padl. All rights reserved.
+//
+
+import Foundation
+import InstantSearch
+
+class PadlHitsController: HitsController {
+    
+}
